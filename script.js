28a970a585852fcf89250da5cf6a16ae1110dfe1
mark <mark.a.obrien00@gmail.com>
2022-08-23 11:30:36 +0400

    create story functionality complete

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 const draggables = document.querySelectorAll(".draggable");
 const containers = document.querySelectorAll(".container");
 
-
-// Changes the style of the element while it is being dragged
-draggables.forEach(draggable => {
-    draggable.addEventListener("dragstart", () => {
-        draggable.classList.add("dragging");
-    })
-
-    draggable.addEventListener("dragend", () => {
-        draggable.classList.remove("dragging");
-    })
-})
-
 // Basic drag and drop 
 containers.forEach(container => {
     container.addEventListener("dragover", (e) => {
@@ -41,3 +29,130 @@ function getDragAFterElement(container, y) {
             return closest;
     }, { offset: Number.NEGATIVE_INFINITY }).element ;
 }
+
+
+
+
+// form
+const colors = document.querySelectorAll(".color");
+const textArea = document.querySelector(".text");
+const create = document.querySelector(".btn-createStory");
+const edits = document.querySelectorAll(".btn-edit");
+
+
+
+
+colors.forEach((color) => {
+    color.addEventListener("click", () => {
+        const textElement = color.parentNode.parentNode.parentElement[0];
+        textElement.style.backgroundColor = window.getComputedStyle(color).backgroundColor;
+    })
+})
+
+const backLog = document.querySelector("#backlog");
+function createStory() {
+    const form = document.createElement("form");
+    const textarea = document.createElement("textarea");
+    const iconArea = document.createElement("div");
+    const edit = document.createElement("img");
+    const editDiv = document.createElement("div");
+    const colors = document.createElement("div");
+    const btnDelete = document.createElement("button");
+
+    form.addEventListener("dragstart", () => {
+        form.classList.add("dragging");
+    })
+
+    form.addEventListener("dragend", () => {
+        form.classList.remove("dragging");
+    })
+
+
+    // settign atributes 
+    form.draggable = true;
+    edit.src = "icons/up-arrow.svg";
+    edit.dataset.direction = "up";
+    edit.draggable = false;
+    btnDelete.type = "button";
+    btnDelete.textContent = "delete";
+
+    
+
+    // adding classes to elements
+    form.classList.add("draggable");
+    textarea.classList.add("text", "text-edit");
+    edit.classList.add("btn-edit", "icon");
+    editDiv.classList.add("edit", "edit-hidden");
+    colors.classList.add("colors");
+
+    function createColors(color) {
+        const btnColor = document.createElement("div");
+        btnColor.type = "button";
+
+        btnColor.style.backgroundColor = color;
+
+
+        btnColor.classList.add("color","btn");
+
+        btnColor.addEventListener("click", () => {
+            const textElement = textarea;
+            textElement.style.backgroundColor = window.getComputedStyle(btnColor).backgroundColor;
+        })
+
+        colors.appendChild(btnColor)
+    }
+
+    createColors("#77BCDE");
+    createColors("#CF3A3E");
+    createColors("#4EB67F");
+    createColors("#D05A2E");
+    createColors("#D1546C");
+
+    // deletes story
+    btnDelete.addEventListener("click", () => {
+        form.remove();
+    })
+
+    
+
+    edit.addEventListener("click", () => {
+        const parentElement = edit.parentNode.parentNode;
+        const textElement = parentElement[0];
+        const editElement = editDiv;
+        console.log(editElement);
+
+
+        // shows and hides the edit 
+        editElement.hidden ? editElement.hidden = false : editElement.hidden = true;
+        editElement.classList.toggle("edit-hidden");
+        textElement.disabled ? textElement.disabled = false : textElement.disabled = true;
+
+        textElement.classList.toggle("text-edit");
+
+        // changes arrow icon
+        if (edit.dataset.direction == "down") {
+            edit.src = "icons/up-arrow.svg";
+            edit.dataset.direction = "up";
+        }
+        else {
+            edit.src = "icons/down-arrow.svg"
+            edit.dataset.direction = "down";
+        };
+    });
+
+
+
+    //appending children
+    iconArea.appendChild(edit);
+    editDiv.append(colors, btnDelete);
+    form.append(textarea, iconArea, editDiv);  
+    backLog.append(form);
+}
+
+create.addEventListener("click", () => {
+    createStory();
+})
+
+
+
+
